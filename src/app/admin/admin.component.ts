2f2dbec4feae50f816a873b2f55a4647d7307f00
auldconsulting <rory@auld.consulting>
2018-05-01 21:32:42 +0100

    nameslist

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DummyDataService } from '../dummy-data.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _service: DummyDataService) { }
+  
   ngOnInit() {
   }
 
