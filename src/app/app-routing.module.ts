d62bc4bfd7effa742742ec3eb8f7b1fff9a195ea
auldconsulting <rory@auld.consulting>
2018-05-01 20:08:44 +0100

    intial and css grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { ClausesComponent } from './clauses/clauses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+   { path: 'admin', component: AdminComponent },
+   { path: 'templates', component: TemplatesComponent },
+   { path: 'clauses', component: ClausesComponent },
+ /* { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent } */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { ClausesComponent } from './clauses/clauses.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    TemplatesComponent,
+    ClausesComponent
   ],
   imports: [
     BrowserModule,
